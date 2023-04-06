30942e1a12adf7c9a17aa5f5aca841e39577d5f6
AmritSd <amritsmail2002@gmail.com>
2023-04-06 05:25:59 -0500

    changes to endpoint

--- app/src/routes/api/complete/+server.js
+++ app/src/routes/api/complete/+server.js
@@ -6,19 +6,23 @@ import { apiKey } from '$env/static/private';
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
-    const { input } = await request.json();
+    const { text } = await request.json();
 
     const configuration = new Configuration({
         apiKey: apiKey
     });
     const openai = new OpenAIApi(configuration);
 
+    console.log(text);
+
     // Call the embedings model text-embedding-ada-002
     const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{role: "user", content: input.text}],
+        messages: [{role: "user", content: text}],
       });
-    //   console.log(completion.data.choices[0].message);
+
+    
+    console.log(completion.data.choices[0].message);
 
     return json({
         data : completion.data.choices[0].message
