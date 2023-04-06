ab71ca413aeee5ff97c4a11e901147dd3843a519
AmritSd <amritsmail2002@gmail.com>
2023-04-06 04:40:09 -0500

    made endpoint for chat-gpt api

---
+++ app/src/routes/api/complete/+server.js
@@ -0,0 +1,26 @@
+// my-backend-function.js
+import { json } from '@sveltejs/kit';
+import { Configuration, OpenAIApi } from "openai";
+import { apiKey } from '$env/static/private';
+
+
+/** @type {import('./$types').RequestHandler} */
+export async function POST({ request }) {
+    const { input } = await request.json();
+
+    const configuration = new Configuration({
+        apiKey: apiKey
+    });
+    const openai = new OpenAIApi(configuration);
+
+    // Call the embedings model text-embedding-ada-002
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{role: "user", content: input.text}],
+      });
+    //   console.log(completion.data.choices[0].message);
+
+    return json({
+        data : completion.data.choices[0].message
+    });
+}
